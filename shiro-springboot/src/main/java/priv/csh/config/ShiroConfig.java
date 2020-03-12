2d542de8e40b66f3750ae794dcefa3622f9f967c
csh <904397420@qq.com>
2020-03-12 14:04:37 +0800

    shiro登录拦截
    1、创建两个新页面用于访问测试，以及login登录页面、修改首页index
    2、新增controller的相关页面跳转
    3、新增shiro登录拦截功能

--- shiro-springboot/src/main/java/priv/csh/config/ShiroConfig.java
+++ shiro-springboot/src/main/java/priv/csh/config/ShiroConfig.java
@@ -14,16 +14,28 @@ public class ShiroConfig {
 
     //shiroFilterFactoryBean 3
     @Bean
-    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultWebSecurityManager defaultWebSecurityManager){
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultWebSecurityManager defaultWebSecurityManager) {
         ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
         // 设置安全管理器
         bean.setSecurityManager(defaultWebSecurityManager);
+
+        //添加shiro的内置过滤器
+        LinkedHashMap<String, String> filterMap = new LinkedHashMap<>();
+
+        //支持通配符的写法，user整个目录下的页面都需要认证
+        filterMap.put("/user/*", "authc");
+
+        //若无权限访问页面，需跳转至登录页面
+        bean.setLoginUrl("/toLogin");
+
+        bean.setFilterChainDefinitionMap(filterMap);
+
         return bean;
     }
 
     //DafaultWebSecurituManager 2
-    @Bean(name="securityManager")
-    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm") UserRealm userRealm){
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm") UserRealm userRealm) {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
 
         //关联UserRealm
@@ -31,9 +43,10 @@ public class ShiroConfig {
 
         return securityManager;
     }
+
     //创建realm对象 ，需要自定义 1
     @Bean(name = "userRealm")
-    public UserRealm userRealm(){
+    public UserRealm userRealm() {
         return new UserRealm();
     }
 
--- shiro-springboot/src/main/java/priv/csh/controller/MyController.java
+++ shiro-springboot/src/main/java/priv/csh/controller/MyController.java
@@ -10,9 +10,26 @@ public class MyController {
     /**
      * 创建访问首页路径,首页不止一个，可以按照以下的写法
      */
-    @RequestMapping({"/","/index","index.html"})
-    public String toIndex(Model model){
-        model.addAttribute("msg","hello");
+    @RequestMapping({"/", "/index", "index.html"})
+    public String toIndex(Model model) {
+        model.addAttribute("msg", "hello");
         return "index"; //跳转到index页面
     }
+
+    @RequestMapping("/user/add")
+    public String add() {
+        return "user/add";
+    }
+
+    @RequestMapping("/user/update")
+    public String update() {
+        return "user/update";
+    }
+
+
+    @RequestMapping("/toLogin")
+    public String toLogin() {
+        return "login";
+    }
+
 }
